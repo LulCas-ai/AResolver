730492b31efa96e2e2e2dc9c4a328010cc8ec3e8
LUCAS IOEL ROITMAN <49518218@ort>
2026-03-17 15:04:28 -0300

    porsilasdudingas

--- script.js
+++ script.js
@@ -1,31 +1,65 @@
-const input = document.getElementById("guessinput")
-const button = document.getElementById("guessBtn")
-const message = document.getElementById("message")
-const attemptsText = document.getElementById("attempts")
+const input = document.getElementById("guessInput");
+const guessButton = document.getElementById("guessBtn");
+const message = document.getElementById("message");
+const attemptsText = document.getElementById("attempts");
+const resetButton = document.getElementById("resetBtn");
+const intentosAnteriores = document.getElementById("intentosAnteriores");
 
-let secretNumber = Math.random() * 100
-let attempts = 0
+let secretNumber = Math.floor(Math.random() * 100);
+let attempts = 0;
+let listaIntentos = [];
 
-button.addEventListener("click", checkGuess())
+input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") guessButton.click();
+});
 
-function checkGuess(){
+guessButton.addEventListener("click", function checkGuess() {
 
-  const guess = input.value
+  const guess = input.value;
 
-  attempts = attempts + 1
+  if (guess < 0 || guess > 100 || guess == "") {
 
-  attemptsText.innerText = attempts
+    message.innerText = "Ingresa un número entre 0 y 100"
 
-  if(guess === secretNumber){
-    message.innerText = "¡Ganaste!"
   }
+  else {
+    attempts += 1;
+    let listaIntentosString = "";
 
-  if(guess < secretNumber){
-    message.innerText = "El número es mayor"
+    attemptsText.innerText = attempts;
+
+    if (guess == secretNumber) {
+      message.innerText = "¡Ganaste!"
+      resetButton.click();
+    }
+
+    else if (guess < secretNumber) {
+      message.innerText = "El número es mayor";
+    }
+
+    else if (guess > secretNumber) {
+      message.innerText = "El número es menor";
+    };
+
+    listaIntentos.push(guess);
+
+    for(i = 0; i < listaIntentos.length; i++){
+      if(i == listaIntentos.length - 1){
+        listaIntentosString += listaIntentos[i];
+      }
+      else{
+        listaIntentosString += listaIntentos[i] + ", ";
+      }
+    };
+
+    intentosAnteriores.innerText = listaIntentosString;
   }
+});
 
-  if(guess > secretNumber){
-    message.innerText = "El número es menor"
-  }
-
-}
+resetButton.addEventListener("click", function restartNumber() {
+  listaIntentos = [];
+  secretNumber = Math.floor(Math.random() * 100);
+  attempts = 0;
+  attemptsText.innerText = 0;
+  input.value = ""
+});
